6eb6bef35928bd00fde3f42be3fbd53bfa121bd4
Chris Cardus <cxc357@student.bham.ac.uk>
2017-11-09 20:52:21 +0000

    Finished CSA Assignment

--- assignment/bub.s
+++ assignment/bub.s
@@ -2,8 +2,8 @@
     .align 2
 
 itemcount:  .asciiz "\nEnter number of integers to be sorted>"
-listitem:       .asciiz "\nEnter integers to be sorted>"
-cr:         .asciiz "\n"
+listitem:   .asciiz "\nEnter integers to be sorted>"
+gap:        .asciiz ", "
 
     .text
     .globl main
@@ -31,7 +31,7 @@ main:
 READ:
     #Check that $s2 is less than itemcount
     sub     $t0, $s2, $s1
-    bgez    $t0, EREAD
+    bgez    $t0, SUBROUTINES
 
     # Read items ready to be sorted in the heap
     li      $v0, 4
@@ -55,43 +55,81 @@ READ:
     j       READ
 
 # End of the READ loop
-EREAD:
-
-    # Reset counter
-    li      $s2, 0
+SUBROUTINES:
+    # Initialise $s3, $s4, $s5
+    jal     RETURN
+    # Bubble Sort Subroutine
+    jal     BUBBLE
     
-    # Store maximum memory address
-    li      $t3, 4
-    mul     $t4, $s1, $t3
-    add     $s3, $t4, $s0
+# Print the sorted list
+PRINT:
+    # Prepare to print some integers
+    li      $v0, 1
+    lw      $a0, ($s4)
+    syscall
+    # Check if we've reached the end of the heap
+    addi    $s4, $s4, 4
+    sub     $t6, $s4, $s3
+    # If so, exit program
+    bgez    $t6, EXIT
+    # Else print a gap
+    li      $v0, 4
+    la      $a0, gap
+    syscall
+    j       PRINT
 
-    # Initialise two addresses to be compared
-    addi	$s4, $s0, 0
-    addi    $s5, $s0, 4
-    
+# End the program
+EXIT:
+    li $v0, 10
+    syscall
     
 # Start of bubble sort
 BUBBLE:
     # Check if $s3 has been reduced to zero
     sub     $t0, $s0, $s3
-    bgez    $t0, EBUBBLE
+    bgez    $t0, RETURN
+
+    # Load the integers at locations $s4 & $s5 from the heap
+    lw      $s6, ($s4)
+    lw      $s7, ($s5)
 
     # Compare two elements
-    sub     $t0, $s5, $s4
+    sub     $t0, $s7, $s6
     bgez    $t0, NOCHANGE
 
-    # Switch $s4 & $s5
-    addi    $t5, $s4, 0
-    move    $s4, $s5
-    move    $s5, $t5
+    # Switch $s6 & $s7 on the heap
+    sw      $s6, ($s5)
+    sw      $s7, ($s4)
 
     NOCHANGE:
     # Increment both $s4 & $s5 to the memory address of the next to items to be compared
     addi    $s4, $s4, 4
     addi    $s5, $s5, 4
 
-# End of bubble sort
-EBUBBLE:
+    # Check if $s5 is the end of the heap
+    sub     $t5, $s5, $s3
+    bgez    $t5, LOCKLAST
 
-    li $v0, 10
-    syscall
+    j BUBBLE
+    
+# Lock the position of the last digit; this digit is sorted
+LOCKLAST:
+    # Reduce the maximum size $s3 by 1 word
+    subi     $s3, $s3, 4
+    addi	$s4, $s0, 0
+    addi    $s5, $s0, 4
+    j BUBBLE
+        
+# Return to the location in $ra
+RETURN:
+    # Store maximum memory address
+    li      $t3, 4
+    mul     $t4, $s1, $t3
+    add     $s3, $t4, $s0
+    
+    # Store addresses of first two items in heap
+    addi	$s4, $s0, 0
+    addi    $s5, $s0, 4
+    
+    # Jump to location $ra
+    jr      $ra
